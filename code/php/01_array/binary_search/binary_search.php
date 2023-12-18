d71bade5082a1f44cee5c4a662c4087b9459b820
lixiaodong <arnold@LAPTOP-0II673P3>
2023-12-18 19:27:27 +0800

    feat: 增加python,php相关代码

---
+++ code/php/01_array/binary_search/binary_search.php
@@ -0,0 +1,71 @@
+<?php
+
+// 二分查找 https://leetcode.cn/problems/binary-search/
+class BinarySearch
+{
+    /**
+     * 左闭右闭
+     *
+     * @param array $nums
+     * @param integer $target
+     * @return integer
+     */
+    public function search1(array $nums, int $target): int
+    {
+        if (count($nums) == 0) {
+            return -1;
+        }
+
+        $left = 0;
+        $right = count($nums) - 1;
+
+        while ($left <= $right) {
+            $middle = $left + ($right - $left) / 2;
+            if ($nums[$middle] > $target) {
+                $right = $middle - 1;
+            } else if ($nums[$middle] < $target) {
+                $left = $middle + 1;
+            } else {
+                return $middle;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 左闭右开
+     *
+     * @param array $nums
+     * @param integer $target
+     * @return integer
+     */
+    public function search2(array $nums, int $target): int
+    {
+        if (count($nums) == 0) {
+            return -1;
+        }
+
+        $left = 0;
+        $right = count($nums);
+
+        while ($left < $right) {
+            $middle = $left + ($right - $left) / 2;
+            if ($nums[$middle] > $target) {
+                $right = $middle;
+            } else if ($nums[$middle] < $target) {
+                $left = $middle + 1;
+            } else {
+                return $middle;
+            }
+        }
+
+        return -1;
+    }
+}
+
+$binarySearch =  new BinarySearch();
+$nums = [-1, 0, 3, 5, 9, 12];
+$target = 9;
+print($binarySearch->search1($nums, $target)."\n");
+print($binarySearch->search2($nums, $target));
---
+++ code/python/01_array/binary_search/binary_search.py
@@ -0,0 +1,63 @@
+import unittest
+from typing import List
+
+
+# 二分查找 https://leetcode.cn/problems/binary-search/
+class BinarySearch:
+    """二分查找"""
+
+    def search1(self, nums: List[int], target: int) -> int:
+        """左闭右闭"""
+        if len(nums) == 0:
+            return -1
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = left + (right - left) // 2
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+
+        return -1
+
+    def search2(self, nums: List[int], target: int) -> int:
+        """左闭右开"""
+        if len(nums) == 0:
+            return -1
+
+        left = 0
+        right = len(nums)
+
+        while left < right:
+            middle = left + (right - left) // 2
+            if nums[middle] > target:
+                right = middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+
+        return -1
+
+
+class TestBinarySearch(unittest.TestCase):
+    def test_search1(self):
+        binary_search = BinarySearch()
+        nums = [-1, 0, 3, 5, 9, 12]
+        target = 9
+        self.assertEqual(binary_search.search1(nums, target), 4)
+
+    def test_search2(self):
+        binary_search = BinarySearch()
+        nums = [-1, 0, 3, 5, 9, 12]
+        target = 9
+        self.assertEqual(binary_search.search2(nums, target), 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
